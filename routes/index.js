ea42cc626cf233902dbe77caee2919f68153d5b7
DJGValls <jiga1980@gmail.com>
2023-02-11 20:18:30 +0100

    Iteration 3 done

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const MovieModel = require("../models/Movie.models");
+
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+/* GET /movies page */
+router.get("/movies", (req, res, next)=>{
+    MovieModel.find()
+    .then((response) => {
+        //console.log(response);
+        res.render("movies.hbs", {
+            allMovies: response
+        })
+    })
+    .catch((error) => {
+        next(error);
+    });
+})
+
+
+
 
 module.exports = router;
